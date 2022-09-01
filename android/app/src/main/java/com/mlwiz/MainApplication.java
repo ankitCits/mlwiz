341dc46bc971ebfeabc0150f0309ac85c19c323d
Ravi Mehta <ravi@mehtawebsolution.com>
2022-09-01 13:30:06 +0530

    Splash screen issue Commented

--- android/app/src/main/java/com/mlwiz/MainApplication.java
+++ android/app/src/main/java/com/mlwiz/MainApplication.java
@@ -12,7 +12,7 @@ import com.facebook.soloader.SoLoader;
 import com.mlwiz.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
+// import org.devio.rn.splashscreen.SplashScreenReactPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -29,7 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new SplashScreenReactPackage());
+          // packages.add(new SplashScreenReactPackage());
           return packages;
 
         }
