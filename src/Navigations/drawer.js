b0b4943c1cd14462c7bee4199c745527c92aacc0
Ravi Mehta <ravi@mehtawebsolution.com>
2022-09-03 18:05:55 +0530

    Icons and Side Drawer

--- src/Navigations/drawer.js
+++ src/Navigations/drawer.js
@@ -3,72 +3,61 @@ import { DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
 
 import { useWindowDimensions, View, StyleSheet } from 'react-native';
 
-function CustomDrawer(props) {
-    const width = useWindowDimensions().width * 0.3;
+import Colors from '../Themes/Colors';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { screenHeight } from '../Themes/Metrices';
 
+function CustomDrawer(props) {
+    const width = useWindowDimensions().width * 0.7;
+    const screens = [
+        { label: 'Dashboard', redirectTo: 'Dashboard', icon: 'home' },
+        { label: 'Explore', redirectTo: 'Explore', icon: 'search' },
+        { label: 'My Events', redirectTo: 'Events', icon: 'calendar' },
+        { label: 'Tasks', redirectTo: 'Tasks', icon: 'list-circle' },
+        { label: 'Invite Friends', redirectTo: 'Invite', icon: 'mail' },
+        { label: 'Settings', redirectTo: 'Settings', icon: 'settings' },
+        { label: 'About', redirectTo: 'About', icon: 'information-circle' },
+    ]
     return (
         <DrawerContentScrollView {...props}>
             <View style={styles.menuContainer}>
-                <View
-                    style={[
-                        styles.menuItemsCard,
-                        { backgroundColor: '#fff2df', width: width, height: width },
-                    ]}>
-                    <>
-                        <View
-                            style={[styles.circleContainer, { backgroundColor: '#FFC56F' }]}>
-                            <DrawerItem
-                                label="Dashboard"
-                                labelStyle={{ color: '#fbae41', fontSize: 10 }}
-                                onPress={() => {
-                                    props.navigation.navigate('Dashboard');
-                                }}
-                            />
-                        </View>
-                    </>
+                {
+                    screens.map((item, key) => {
+                        return (
+                            <View style={{ flexDirection: 'row' }}>
+                                <DrawerItem
+                                    key={key}
+                                    style={{ width: width }}
+                                    icon={({ focused, color, size }) => <Icon color={color} size={size} name={focused ? `${item.icon}` : `${item.icon}-outline`} />}
+                                    label={`${item.label}`}
+                                    labelStyle={{ color: Colors.BLACK, fontWeight: '700' }}
+                                    onPress={() => props.navigation.navigate(`${item.redirectTo}`)} />
+                            </View>
+                        );
+                    })
+                }
+
+                <View style={{ flex: 1, justifyContent: 'flex-end', marginBottom: 60 }}>
                     <DrawerItem
-                        style={{
-                            position: 'absolute',
-                            left: 0,
-                            width: width,
-                            height: width,
-                        }}
-                        label="Dashboard"
-                        labelStyle={{ color: '#609806' }}
-                        onPress={() => {
-                            props.navigation.navigate('Dashboard');
-                        }}
-                    />
+                        style={{ width: width }}
+                        icon={({ focused, color, size }) => <Icon color={color} size={size} name={focused ? `exit` : `exit-outline`} />}
+                        label={`Sign Out`}
+                        labelStyle={{ color: Colors.BLACK, fontWeight: '700' }}
+                        onPress={() => console.log('Signout')} />
                 </View>
 
             </View>
-        </DrawerContentScrollView>
+        </DrawerContentScrollView >
     );
 }
 
 export default CustomDrawer;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+
     menuContainer: {
         flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
+        height: screenHeight(100),
     },
-    menuItemsCard: {
-        // flexDirection: 'column',
-        // justifyContent: 'center',
-        // alignItems: 'center',
-        // borderRadius: 10,
-    },
-    circleContainer: {
-        // width: 50,
-        // height: 50,
-        // borderRadius: 25,
-        // padding: 10,
-    },
+
 });
