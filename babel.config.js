84b90c6a8c331691e0b3d65bc72cc4ab86ac9684
Ravi Mehta <ravi@mehtawebsolution.com>
2022-09-05 12:03:33 +0530

    Babel Fix

--- babel.config.js
+++ babel.config.js
@@ -4,8 +4,23 @@ module.exports = {
     [
       "module-resolver",
       {
-        extensions: [".tsx", ".ts", ".js", ".json",".cjs",".graphql"],
-      },
+        "root": ["./src"],
+        "alias": [
+          // { "@shared-components": "./shared/components" },
+          // { "@shared-constants": "./shared/constants" },
+          // { "@shared-theme": "./shared/theme" },
+          // { "@font-size": "./shared/theme/font-size" },
+          // { "@api": "./services/api/index" },
+          // { "@fonts": "./shared/theme/fonts/index" },
+          // { "@colors": "./shared/theme/colors" },
+          // { "@theme": "./shared/theme/index" },
+          // { "@services": "./services" },
+          // { "@screens": "./screens" },
+          // { "@utils": "./utils/" },
+          // { "@assets": "./assets/" }
+        ],
+        "extensions": [".tsx", ".ts", ".js", ".json", ".cjs", ".graphql"]
+      }
     ],
     "react-native-reanimated/plugin",
   ],
