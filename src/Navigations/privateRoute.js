cda609bfc64535120dcb61f0dab3c2d2f9e70064
office2 <office2@mehtawebsolution.com>
2022-09-05 19:02:49 +0530

    private route add page edit notification

--- src/Navigations/privateRoute.js
+++ src/Navigations/privateRoute.js
@@ -5,6 +5,7 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import Incidents from '../Screens/private/Incidents';
 import CustomDrawer from './drawer';
 import Profile from '../Screens/private/Profile';
+import EditNotification from '../Screens/private/EditNotification';
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
@@ -18,13 +19,17 @@ const drawerArray = [
         name: 'Profile',
         component: props => <Profile {...props} />,
     },
+    {
+        name: 'EditNotification',
+        component: props => <EditNotification {...props} />,
+    },
 ];
 
 const PrivateRoute = props => {
     return (
         <Drawer.Navigator
             headerMode="none"
-            initialRouteName="Profile"
+            initialRouteName="EditNotification"
             drawerContent={(props) => <CustomDrawer {...props} />}
         >
             {drawerArray.map((item, index) => {
