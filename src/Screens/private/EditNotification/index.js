6505f7d929a33485f61ff423ef817ac5d2979215
developermw-5 <63443708+developermw-5@users.noreply.github.com>
2022-09-06 14:19:21 +0530

    fix modal design

--- src/Screens/private/EditNotification/index.js
+++ src/Screens/private/EditNotification/index.js
@@ -1,12 +1,20 @@
-import React from "react";
-import { Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import React, { useState } from "react";
+import { Text, View, SafeAreaView, TouchableOpacity, Modal } from 'react-native';
 import { styles } from './styles';
 import Colors from '../../../Themes/Colors';
 import Header from '../../../Components/header';
 import { ScrollView } from "react-native-gesture-handler";
 import Icon from 'react-native-vector-icons/Ionicons';
+import Input from '../../../Components/inputs';
 
 const EditNotification = (props) => {
+
+    const [visible, setVisible] = useState(false);
+    const [value, setValues] = useState(0);
+    const showRuleModal = () => {
+        setVisible(true);
+    }
+
     return (
         <SafeAreaView>
             <Header navigation={props.navigation} title={'Edit Notifications'} showBack={true} showFilter={false} showSearch={false} />
@@ -81,14 +89,59 @@ const EditNotification = (props) => {
                     <Text style={styles.subText}>Add new rule at 0 minutes</Text>
                 </View>
 
-                <View style={styles.bottomContainer}>
-
-                </View>
-                <View>
-                <Text style={{color:Colors.BLUE}}>Add a new rule</Text>
-                </View>
-               
-
+                <TouchableOpacity
+                    onPress={() => { showRuleModal() }}
+                    style={styles.bottomContainer}>
+                    <Text>Add a new rule</Text>
+                </TouchableOpacity>
+                <Modal visible={visible}
+                    transparent={true}
+                    onPress={() => setVisible(false)}
+                    animationType="none"
+                >
+                    <View style={styles.modalContainer}>
+                        <View style={{ flexDirection: 'row', alignContent: "space-between" }}>
+                            <Text style={styles.modalTitle}>Edit rule</Text>
+                            <TouchableOpacity
+                                style={styles.modalClose}
+                                onPress={() => setVisible(false)}
+                            >
+                                <Icon name={'close'} size={16} />
+                            </TouchableOpacity>
+                        </View>
+                        <Input
+                            labelText={'At minute'}
+                            value={value}
+                            type={'numeric'}
+                            onChangeText={text => setValues(text)}
+                            placeholder={''}
+                        />
+                        <Text style={styles.modalTitle}>Notify via</Text>
+                        <View style={styles.modalDetail}>
+                            <Text style={styles.detailsText}>Call (+91 9098481515)</Text>
+                            <Text style={styles.detailsText}>Message (+91 9098481515)</Text>
+                            <Text style={styles.detailsText}>Email (aiwizmobile1@gmail.com)</Text>
+                        </View>
+                        <View style={styles.modalBottom}>
+                            <Text style={styles.modalBottomText}>Push (To All Devices) </Text>
+                        </View>
+                        <View style={styles.btnContainer}>
+                            <View style={{}}>
+                                <View style={styles.trashContainer}>
+                                    <Icon name={'trash'} size={16} />
+                                </View>
+                            </View>
+                            <View style={{ flex: 1 }}>
+                                <View style={styles.rightContainer}>
+                                    <Icon name={'md-pencil'} size={16} />
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
+                {/* <View style={styles.logoutContainer}>
+                    <Text style={[styles.textLight, { textDecorationStyle: 'solid' }]}>Sign Out</Text>
+                </View> */}
             </ScrollView>
         </SafeAreaView>
     )
--- src/Screens/private/EditNotification/styles.js
+++ src/Screens/private/EditNotification/styles.js
@@ -8,46 +8,113 @@ const styles = StyleSheet.create({
         marginTop: 10,
         flexDirection: 'row',
         alignItems: 'center',
-        backgroundColor:Colors.BLUE_LIGHT,
-        width:screenWidth(100),
-        height:20,
-        
+        backgroundColor: Colors.BLUE_LIGHT,
+        width: screenWidth(100),
+        height: 20,
+
     },
-    ItemContainer:{
-        paddingLeft:15,
-        paddingHorizontal:10,
-        marginTop:10,
+    ItemContainer: {
+        paddingLeft: 15,
+        paddingHorizontal: 10,
+        marginTop: 10,
     },
     textLight: {
         marginTop: 15,
         color: Colors.DARK_GREY,
         fontSize: 16,
-        
+
     },
     itemTitle: {
         color: Colors.GREY3,
         fontSize: 13,
-        
+
     },
     toggleContainer: {
         flex: 1,
         alignSelf: 'center',
         alignItems: 'flex-end',
-        marginHorizontal:30
+        marginHorizontal: 30
     },
     logoutContainer: {
         marginTop: 40
     },
-    bottomContainer:{
-        paddingTop:20,  
-        paddingLeft:10, 
-        marginTop:40,
-        color:Colors.BLUE,
+    bottomContainer: {
+        paddingTop: 20,
+        paddingLeft: 10,
+        marginTop: 40,
+        color: Colors.BLUE,
     },
-    subText:{
-        paddingTop:9,
-        color:Colors.BLUE,
+    subText: {
+        paddingTop: 9,
     },
-
+    modalContainer: {
+        backgroundColor: '#FFFFFF',
+        position: 'absolute',
+        bottom: 0,
+        paddingTop: screenHeight(3),
+        padding: screenHeight(1),
+        borderTopLeftRadius: 15,
+        borderTopRightRadius: 15,
+        elevation: 1,
+        width: screenWidth(100),
+        height: screenHeight(60),
+    },
+    modalTitle: {
+        marginBottom: 15,
+        margin: 5,
+        fontSize: 18,
+        backgroundColor: '#FFFFFF',
+        color: '#D9D9D9'
+    },
+    modalClose: {
+        alignSelf: "flex-start",
+        flex: 1,
+        justifyContent: "flex-end",
+        alignContent: "center",
+        alignItems: "flex-end",
+        flexDirection: "row"
+    },
+    modalDetail: {
+        margin: 6,
+    },
+    detailsText: {
+        color: '#A1BBDC',
+        fontSize: 16,
+        margin: screenHeight(1),
+    },
+    modalBottom: {
+        marginHorizontal: screenHeight(0.5),
+        padding: screenHeight(1.5),
+        backgroundColor: '#F3F6FB',
+        borderRadius: 5,
+    },
+    modalBottomText: {
+        color: '#99B1D8',
+        fontSize: 17,
+        fontWeight: '500',
+    },
+    btnContainer: {
+        flexDirection: 'row',
+        paddingTop: 15,
+    },
+    trashContainer: {
+        backgroundColor: '#FDF1F4',
+        marginVertical: 10,
+        marginLeft: 10,
+        padding: screenWidth(5),
+        width: screenWidth(15),
+        alignItems: 'center',
+        borderRadius: 35,
+    },
+    rightContainer: {
+        backgroundColor: '#F3F6FB',
+        marginVertical: 10,
+        marginHorizontal: 5,
+        padding: screenWidth(5),
+        width: screenWidth(15),
+        borderRadius: 35,
+        alignSelf: 'flex-end',
+        alignItems: 'center'
+    },
 });
 export { styles };
